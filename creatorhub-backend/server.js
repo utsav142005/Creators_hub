00bcd999ec404abf966d915efd92ad4cde7bfc82
utsav142005 <utsavtalreja2005@gmail.com>
2026-04-11 14:13:43 +0530

    Update server.js

--- creatorhub-backend/server.js
+++ creatorhub-backend/server.js
@@ -8,7 +8,6 @@ require('dotenv').config();   // Load .env FIRST before anything else
 
 const express    = require('express');
 const cors       = require('cors');
-const helmet     = require('helmet');
 const morgan     = require('morgan');
 const rateLimit  = require('express-rate-limit');
 
